c0c14f32e2c52d3cf6cc5d0eb84f4457a8173865
Maltsev Pavel <pavel.maltsev@infotecs.ru>
2015-08-20 12:48:44 +0300

    Initial commit

---
+++ Sicp/LispSimpleCompiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Sicp
+{
+    public class LispSimpleCompiler
+    {
+        public static int Execute(string expression)
+        {
+            ParsedExpression parsedExpression = Parse(expression);
+
+            return ExecuteCore(parsedExpression);
+        }
+
+        private static int ExecuteCore(ParsedExpression parsedExpression)
+        {
+            if (parsedExpression.Operator == "+")
+            {
+                return int.Parse(parsedExpression.Arg1) + int.Parse(parsedExpression.Arg2);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private static ParsedExpression Parse(string expression)
+        {
+            string[] parts = SplitEpression(expression);
+
+            return new ParsedExpression
+            {
+                Operator = parts[0], Arg1 = parts[1], Arg2 = parts[2],
+            };
+        }
+
+        private static string[] SplitEpression(string expression)
+        {
+            if (expression[0] != '(' || expression[expression.Length - 1] != ')')
+            {
+                throw new ArgumentException("Incorrect expression");
+            }
+
+            return expression.Substring(1, expression.Length - 2).Split(' ');
+        }
+    }
+
+    internal sealed class ParsedExpression
+    {
+        public string Arg1 { get; set; }
+        public string Arg2 { get; set; }
+        public string Operator { get; set; }
+    }
+}
---
+++ Sicp/Tests/LispSimpleCompilerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Sicp.Tests
+{
+    public sealed class LispSimpleCompilerTests
+    {
+        [Fact]
+        public void Add()
+        {
+            int result = LispSimpleCompiler.Execute("(+ 2 3)");
+
+            Assert.Equal(5, result);
+        }
+    }
+}
