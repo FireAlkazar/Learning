4f442a7e6e45fea508c45fecf59dacd99d253ec5
User <1@some.org>
2015-08-21 22:48:57 +0300

    Logarithmic multiplication

---
+++ Sicp/BookExercises/LogarithmicMultiplicationProblem_1_18.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Sicp.BookExercises
+{
+    public class LogarithmicMultiplicationProblem_1_18
+    {
+        [Fact]
+        public void MultiplyTest()
+        {
+            int result = Multiply(6, 7);
+
+            Assert.Equal(42, result);
+        }
+
+        private int Multiply(int p1, int p2)
+        {
+            int rests = 0;
+            while (p1 != 1)
+            {
+                if (p1 % 2 == 0)
+                {
+                    p1 /= 2;
+                    p2 *= 2;
+                }
+                else 
+                {
+                    rests += p2;
+                    p1 -= 1;
+                }
+            }
+
+            return p2 + rests;
+        }
+    }
+}
