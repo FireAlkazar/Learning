15d3035a80d2838ff04ff175beaa15f4b6bf5044
Maltsev Pavel <fire.alkazar@gmail.com>
2015-08-24 23:26:57 +0300

    Chapter 2 execises: different pair representations

---
+++ Sicp/BookExercises/Chapter2/PairAsSingleNumber_2_5.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Sicp.BookExercises.Chapter2
+{
+    public class PairAsSingleNumber_2_5
+    {
+        [Fact]
+        public void Test()
+        {
+            var pair = Cons(4, 8);
+
+            Assert.Equal(4, Car(pair));
+            Assert.Equal(8, Cdr(pair));
+        }
+
+        /// <summary>
+        /// Get second pair element;
+        /// </summary>
+        private int Cdr(int pair)
+        {
+            return CdrIter(pair, 0);
+        }
+
+        private int CdrIter(int pair, int val)
+        {
+            if (pair % 3 != 0)
+            {
+                return val;
+            }
+
+            return CdrIter(pair / 3, val + 1);
+        }
+
+        /// <summary>
+        /// Get first element in pair
+        /// </summary>
+        private int Car(int pair)
+        {
+            return CarIter(pair, 0);
+        }
+
+        private int CarIter(int pair, int val)
+        {
+            if (pair % 2 != 0)
+            {
+                return val;
+            }
+
+            return CarIter(pair / 2, val + 1);
+        }
+
+        /// <summary>
+        /// Make-pair
+        /// </summary>
+        private int Cons(int p1, int p2)
+        {
+            return (int)(Math.Pow(2, p1) * Math.Pow(3, p2));
+        }
+
+
+    }
+}
---
+++ Sicp/BookExercises/Chapter2/PairsAsLambda_2_4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Sicp.BookExercises.Chapter2
+{
+    public class PairsAsLambda_2_4
+    {
+        [Fact]
+        public void PairTest()
+        { 
+            var pair = Cons(3,4);
+
+            Assert.Equal(3, Car(pair));
+            Assert.Equal(4, Cdr(pair));
+        }
+
+        /// <summary>
+        /// Get second pair element;
+        /// </summary>
+        private int Cdr(Func<Func<int, int, int>, int> pair)
+        {
+            return pair((x, y) => y);
+        }
+
+        /// <summary>
+        /// Get first element in pair
+        /// </summary>
+        private int Car(Func<Func<int, int, int>, int> pair)
+        {
+            return pair((x, y) => x);
+        }
+
+        /// <summary>
+        /// Make-pair
+        /// </summary>
+        private Func<Func<int, int, int>, int> Cons(int p1, int p2)
+        {
+            Func<Func<int, int, int>, int> result = m => m(p1, p2);
+            return result;
+        }
+    }
+}
