0d26a5eb30fd078934ef5739aef435fdfaee368a
Maltsev Pavel <fire.alkazar@gmail.com>
2015-10-26 22:12:35 +0300

    ~Exercise 2-91

--- Fsharp/Polynomials_2_91.fsx
+++ Fsharp/Polynomials_2_91.fsx
@@ -13,8 +13,6 @@ let restTerms termList = List.tail termList
 let isEmptyTermList termList = List.length termList = 0
 let makeTerm order coeff = [order;coeff]
 
-
-
 let rec addTerms l1 l2 = 
     if isEmptyTermList l1 then l2
     elif isEmptyTermList l2 then l1
@@ -41,38 +39,25 @@ let mulOnMinusOne L =
 let subTerms L1 L2 =
     addTerms L1 (mulOnMinusOne L2)
 
+let rec divTerms l1 l2 =
+    if isEmptyTermList l1 then [theEmptyTermList;theEmptyTermList]
+    else
+        let t1 = firstTerm l1
+        let t2 = firstTerm l2
+        if getOrder(t1) < getOrder(t2) then [[];l1]
+        else
+            let newC = (coeff t1) / (coeff t2)
+            let newO = getOrder(t1) - getOrder(t2)
+            let curTerm = makeTerm newO newC
+            let restOfResult = divTerms (subTerms l1 (mulTerms [curTerm] l2)) l2
+            let result = adjoinTerm curTerm restOfResult.[0]
+            [result; restOfResult.[1]]
+
 let m1 = makeTerm 3 1
 let m2 = makeTerm 2 1
+let c1 = makeTerm 0 -1
+let m3 = addTerms [m2] [c1]
 
-let mm = subTerms [m1] [m2]
+let mm = divTerms [m1] m3
 printf "%A" mm
-
-
-//let term1 = makeTerm 3 1
-//let terms = adjoinTerm (makeTerm 4 1) [(makeTerm 3 3)]
-//let res = addTerms [term1] terms
-//printfn "%A" res
-//
-//let makePoly var terms = (var,terms)
-//let getVariable poly = 
-//    let (a,b) = poly
-//    a
-//let getTermList poly = 
-//    let (a,b) = poly
-//    b
-//let isVariable v = System.Char.IsLetter(v)
-//let isSameVariable (v1:char, v2:char) = v1 = v2
-//
-//let rec divTerms l1 l2 =
-//    if isEmptyTermList l1 then (theEmptyTermList,theEmptyTermList)
-//    else
-//        let t1 = firstTerm l1
-//        let t2 = firstTerm l2
-//        if getOrder(t1) > getOrder(t2) then ([],l1)
-//        else
-//            let newC = (coeff t1) / (coeff t2)
-//            let newO = getOrder(t1) - getOrder(t2)
-//            let restOfResult = divTerms 
-//
-//makePoly 'x' terms
 
