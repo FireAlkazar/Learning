809cc4c3ef85a952d48073c5cdc657acb7f31c04
Maltsev Pavel <fire.alkazar@gmail.com>
2015-10-29 16:52:49 +0300

    ~ Exercise 3.2

---
+++ Fsharp/FunctionCallCounter_3_2.fsx
@@ -0,0 +1,33 @@
+﻿type MonitoredArgType = 
+    | Str of string
+    | Float of float
+
+let makeMonitored (f : float -> float) =
+    let count = ref 0.0
+    fun (x:MonitoredArgType) -> 
+        match x with
+            | Str str  ->
+                match str with
+                    | "showCalls" -> !count
+                    | "resetCalls" ->
+                        count := 0.0
+                        !count
+                    | _ -> 
+                        printfn "error"
+                        800001.0
+            | Float y -> 
+                count := !count + 1.0
+                f y
+
+let monitoredSqrt = makeMonitored System.Math.Sqrt
+let arg1 = Float(100.0)
+let arg2 = Str("showCalls")
+let arg3 = Str("resetCalls")
+
+let sqrtOf100 = monitoredSqrt arg1
+let callsNum = monitoredSqrt arg2
+let afterResetCalls = monitoredSqrt arg3
+//monitoredSqrt arg3
+//monitoredSqrt arg2
+
+
