1fa5d3375533979135ee817b26a243be2338de50
Maltsev Pavel <fire.alkazar@gmail.com>
2015-10-30 19:36:44 +0300

    +Exercise 3-14

---
+++ Fsharp/Mystery-function-3-14.fsx
@@ -0,0 +1,15 @@
+﻿let mystery (x : 'a list) =
+    let rec loop x y =
+        if List.isEmpty x then y
+        else
+            let temp = List.tail x
+            let newX = List.head x :: y
+            loop temp newX
+    loop x List.empty
+
+let argList = ['a';'b';'c';'d']
+
+let result = mystery argList
+
+//so function make reverse
+//and in original version also reverses argList
