ba5a33402ad21e6f6f446ecbd98a67c0c4193e9d
Maltsev Pavel <fire.alkazar@gmail.com>
2015-11-04 22:42:00 +0300

    ~Exercise 3-25 started

---
+++ Fsharp/MultiKeyTable_3_25.fsx
@@ -0,0 +1,25 @@
+﻿// insert ["a";"b"] 'c'
+// lookup ["a";"b"] 
+open System
+//simple table
+let makeSimpleTable() = 
+    let list = new Collections.Generic.List<obj>()
+    list.Insert(0, "*Table*")
+    list
+
+let insertSimple (table : Collections.Generic.List<obj>) (key : obj) (value:obj) =
+    if table.Contains(key) then table.Remove((key,value)) |> ignore
+    table.Insert(1,(key,value))
+
+let lookupSimple (table: Collections.Generic.List<obj>) (key:obj) =
+    let getKey (y:obj) = 
+        if y = box "*Table*" then new obj()
+        else
+            let (a,b) = y :?> obj*obj
+            a
+    table.Find(fun x -> getKey(x) = key)
+
+let simpTable = makeSimpleTable()
+let obj1 = new System.Object()
+insertSimple simpTable "a" 1
+let res = lookupSimple simpTable "a"
