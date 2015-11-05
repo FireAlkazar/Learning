726b4bb62f0ff94c13b422178ec87dbd5821dc82
Maltsev Pavel <fire.alkazar@gmail.com>
2015-11-05 15:48:32 +0300

    *more exercise 3-25

--- Fsharp/MultiKeyTable_3_25.fsx
+++ Fsharp/MultiKeyTable_3_25.fsx
@@ -23,3 +23,26 @@ let simpTable = makeSimpleTable()
 let obj1 = new System.Object()
 insertSimple simpTable "a" 1
 let res = lookupSimple simpTable "a"
+
+//Two key table
+let makeTwoKeyTable() =
+    let dict = new Collections.Generic.Dictionary<obj,Collections.Generic.Dictionary<obj,obj>>()
+    dict
+
+let insertByTwoKey (dict : Collections.Generic.Dictionary<obj,Collections.Generic.Dictionary<obj,obj>>) k1 k2 value =
+    if dict.ContainsKey(k1) then dict.[k1].[k2] <- value
+    else 
+        dict.[k1] <- new Collections.Generic.Dictionary<obj,obj>()
+        dict.[k1].[k2] <- value
+
+let lookupByTwoKey (dict : Collections.Generic.Dictionary<obj,Collections.Generic.Dictionary<obj,obj>>) k1 k2 =
+    if dict.ContainsKey(k1) then
+        let subDict = dict.[k1]
+        if subDict.ContainsKey(k2) then subDict.[k2]
+        else null
+    else null
+
+let dict = makeTwoKeyTable()
+insertByTwoKey dict "subject" "math" 1
+insertByTwoKey dict "subject" "biology" "lion"
+let res2 = lookupByTwoKey dict "subject" "biology"
