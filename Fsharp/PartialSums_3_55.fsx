db06eb4f832e9cbc6ec38d137fbce393cf07fc64
Maltsev Pavel <fire.alkazar@gmail.com>
2015-11-09 23:41:47 +0300

    + Exercise 3-55

---
+++ Fsharp/PartialSums_3_55.fsx
@@ -0,0 +1,9 @@
+﻿let integers = Seq.initInfinite (fun x -> x + 1)
+
+let rec partialSums s = 
+    let rec parSums = seq { yield Seq.head s; yield! Seq.initInfinite (fun x -> (parSums |> Seq.nth x ) + (s |> Seq.nth (x + 1) )) }
+    parSums
+
+let integerPartialSums = partialSums integers
+
+let rez = integerPartialSums |> Seq.take 5 |> Seq.toList
