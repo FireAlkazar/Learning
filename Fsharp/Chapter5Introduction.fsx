a3f956839fa32010269e85c56a36e1909058fb37
Maltsev Pavel <fire.alkazar@gmail.com>
2015-11-17 00:58:42 +0300

    *Chapter5 code updated

--- Fsharp/Chapter5Introduction.fsx
+++ Fsharp/Chapter5Introduction.fsx
@@ -4,62 +4,40 @@
 
 let test1 = gcd 50 40
 
-type ValueSource = 
-    | Constant of int
-    | RegisterRef of string
-    | OperationRef of string
-
-type Button =
-    {
-        Name : string
-        Source : ValueSource
-    }
-
-type Register = 
-    { 
-        Name : string
-        Buttons : Button list
-    }
-
-let dataPaths =
-    [
-        { Name = "a"; Buttons = [{ Name = "a<-b"; Source = RegisterRef("b")}]}
-        { Name = "b"; Buttons = [{ Name = "b<-t"; Source = RegisterRef("t")}]}
-        { Name = "t"; Buttons = [{ Name = "t<-r"; Source = OperationRef("rem")}]}
-    ]
+type Function = | Predicate of  (int -> int -> bool)
+                | Calc of (int -> int -> int)
 
 type Operation = 
     {
-        Name : string
-        Inputs : ValueSource list
+        Name: string
+        Func: Function 
     }
 
-let operations = 
-    [
-        { Name = "rem"; Inputs = [RegisterRef("a"); RegisterRef("b")]}
-        { Name = "="; Inputs = [RegisterRef("b"); Constant(0)]}
-    ]
+let makeMachine (registers: string list) (operations: Operation list) (controller : string list) =
+    failwith("ni") |> ignore
 
-let controller =
-    [
-        "test-b"
-        "test ="
-        "branch label gcd-done"
-        "t<-r"
-        "a<-b"
-        "b<-t"
-        "goto label test-b"
-        "gcd-done"
-    ]
-
-let controller2 =
+let gcdRegisters = ["a";"b";"t"]
+let gcdOperations =
+     [
+        {Name="rem"; Func=Calc(fun x y -> x % y)}
+        {Name="="; Func=Predicate(fun x y -> x = y)}
+     ]
+let gcdController = 
     [
         "test-b"
         "test (op =) (reg b) (const 0)"
-        "branch label gcd-done"
+        "branch (label gcd-done)"
         "assign t (op rem) (reg a) (reg b)"
         "assign a (reg b)"
         "assign b (reg t)"
-        "goto label test-b"
+        "goto (label test-b)"
         "gcd-done"
     ]
+
+let gcdMachine =
+    makeMachine gcdRegisters  gcdOperations gcdController
+
+//setRegister gcdMachine "a" 206
+//setRegister gcdMachine "b" 40
+//start gcdMachine
+//let result = getRegister gcdMachine "a"
