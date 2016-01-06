1190c369db5aff3eb512509e56c5a89efc808235
Maltsev Pavel <fire.alkazar@gmail.com>
2016-01-06 16:25:42 +0300

    Fitnesse: wrapping business objects

---
+++ Fitnesse/AcceptanceTests/MessageLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTests
+{
+	/// <summary>
+	/// Used to demonstrate business object wrapping
+	/// 
+	/// This feature is very useful for functional tests, but is not so well suited tocustomer-oriented story tests.
+	/// </summary>
+	public class MessageLog : fitlibrary.DoFixture
+	{
+		Queue<string> queue = new Queue<string>();
+		public MessageLog()
+		{
+			mySystemUnderTest = queue;//!!!!!
+		}
+		public void GenerateMessages(int count)
+		{
+			for (int i = 0; i < count; i++)
+				queue.Enqueue("M" + i);
+		}
+	}
+}
