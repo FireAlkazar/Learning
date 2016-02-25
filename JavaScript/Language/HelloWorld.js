115a1f756fc65818822c2ffd7bd5f53856893042
Maltsev Pavel <fire.alkazar@gmail.com>
2016-02-25 23:52:45 +0300

    + Js hello world

---
+++ JavaScript/Language/HelloWorld.js
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML/>
+<html>
+<head>
+  <meta charset="utf-8"/>
+</head>
+
+
+
+<script type='text/html'>
+	alert('Hello world')
+</script>
+
+<p>
+Some text
+</p>
+
+<script >
+	alert('ß - javascript')
+</script>
+</html>
