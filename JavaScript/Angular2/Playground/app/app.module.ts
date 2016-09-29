6e82956bf2c6076a9efce31aa7f11eb23e5cb2a7
Maltsev Pavel <fire.alkazar@gmail.com>
2016-09-30 01:48:11 +0300

    angular2 tutorial

--- JavaScript/Angular2/Playground/app/app.module.ts
+++ JavaScript/Angular2/Playground/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
