d8da0f67c4c20fbb6685e6d4fffe791c401ea169
Maltsev Pavel <fire.alkazar@gmail.com>
2016-10-03 22:47:41 +0300

    angular2 tutorial http module

--- JavaScript/Angular2/Playground/app/app.module.ts
+++ JavaScript/Angular2/Playground/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule } 	from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
@@ -12,6 +18,8 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+	HttpModule,
+	InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
