d5c5f4d6638676f6509b2427e1100e4e19002fe2
Maltsev Pavel <fire.alkazar@gmail.com>
2017-01-12 23:00:42 +0300

    * TravelCards sort small refactoring

--- Sicp/TravelingDomain/PathService.cs
+++ Sicp/TravelingDomain/PathService.cs
@@ -13,30 +13,14 @@ namespace TravelingDomain
                 throw new ArgumentException(nameof(cards));
             }
 
-            TravelCard firstCard = GetFirstCard(cards);
-            List<TravelCard> orderedCards = GetOrderCardsByMovingFromFirstToLast(firstCard, cards);
+            var firstCard = GetFirstCard(cards);
+            var orderedCards = GetOrderedCardsByMovingFromFirstToLast(firstCard, cards);
             return orderedCards;
-        }
-
-        private List<TravelCard> GetOrderCardsByMovingFromFirstToLast(TravelCard firstCard, List<TravelCard> cards)
-        {
-            var result = new List<TravelCard> { firstCard };
-            var dictionary = cards.ToDictionary(x => x.DeparturePoint, x => x);
-            TravelCard currentCard = firstCard;
-
-            while (dictionary.ContainsKey(currentCard.DestinationPoint))
-            {
-                var nextCard = dictionary[currentCard.DestinationPoint];
-                result.Add(nextCard);
-                currentCard = nextCard;
-            }
-
-            return result;
         }
 
         private TravelCard GetFirstCard(List<TravelCard> cards)
         {
-            var orderedDestinationPoints = cards
+            string[] orderedDestinationPoints = cards
                 .Select(x => x.DestinationPoint)
                 .OrderBy(x => x)
                 .ToArray();
@@ -49,12 +33,28 @@ namespace TravelingDomain
                 }
             }
 
-            throw new IndexOutOfRangeException("First card not found");
+            throw new InvalidOperationException("First card not found");
+        }
+
+        private List<TravelCard> GetOrderedCardsByMovingFromFirstToLast(TravelCard firstCard, List<TravelCard> cards)
+        {
+            var result = new List<TravelCard> {firstCard};
+            var dictionary = cards.ToDictionary(x => x.DeparturePoint, x => x);
+            var currentCard = firstCard;
+
+            while (dictionary.ContainsKey(currentCard.DestinationPoint))
+            {
+                var nextCard = dictionary[currentCard.DestinationPoint];
+                result.Add(nextCard);
+                currentCard = nextCard;
+            }
+
+            return result;
         }
 
         private bool HasNoCardBefore(TravelCard travelCard, string[] orderedDestinationPoints)
         {
-            var elementIndex = Array.BinarySearch(orderedDestinationPoints, travelCard.DeparturePoint);
+            int elementIndex = Array.BinarySearch(orderedDestinationPoints, travelCard.DeparturePoint);
             return elementIndex < 0;
         }
     }
