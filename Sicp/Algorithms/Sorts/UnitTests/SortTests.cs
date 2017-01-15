013c99899ae818d65c5923500191987c8fb6a0f3
Maltsev Pavel <fire.alkazar@gmail.com>
2017-01-15 12:38:48 +0300

    + Selection sort

---
+++ Sicp/Algorithms/Sorts/UnitTests/SortTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Algorithms.Sorts.UnitTests
+{
+    public class SortTests
+    {
+        [Theory]
+        [InlineData(new int[0], new int[0])]
+        [InlineData(new[] { 1 }, new[] { 1})]
+        [InlineData(new[] { 1,2 }, new[] { 1,2})]
+        [InlineData(new[] { 3, 2, 1 }, new[] { 1, 2, 3})]
+        [InlineData(new[] { 4, 3, 2, 1 }, new[] { 1, 2, 3, 4})]
+        public void Sort_Array_Ok(int[] array, int[] expectedResult)
+        {
+            var selectionSort = new SelectionSort();
+
+            selectionSort.Sort(array);
+
+            Assert.Equal(expectedResult, array);
+        }
+    }
+}
