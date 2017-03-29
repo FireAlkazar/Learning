d72f322d0d7c102f7651147955087db31417cee0
Maltsev Pavel <fire.alkazar@gmail.com>
2017-03-29 21:33:50 +0300

    Tokenizer

---
+++ Sicp/Sicp/Lisp/Tokenizer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Sicp.Lisp
+{
+    public enum TokenType
+    {
+        Define,
+        Plus,
+        Int,
+        Identifier
+    }
+
+    public class Token
+    {
+        public TokenType Type { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public class Tokenizer
+    {
+        private static readonly List<KeyValuePair<string,TokenType>> regexTable = new List<KeyValuePair<string, TokenType>>
+        {
+            new KeyValuePair<string, TokenType>(@"^define", TokenType.Define),
+            new KeyValuePair<string, TokenType>(@"^\+", TokenType.Plus),
+            new KeyValuePair<string, TokenType>(@"^\d+", TokenType.Int),
+            new KeyValuePair<string, TokenType>(@"^[a-zA-Z0-9]", TokenType.Identifier),
+        }; 
+
+         public List<Token> Parse(string program)
+         {
+            var result = new List<Token>();
+
+             string[] lines = program.Split(new [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+             foreach (var line in lines)
+             {
+                List<Token> lineTokens = ParseLine(line);
+                result.AddRange(lineTokens);
+             }
+
+             return result;
+         }
+
+        private List<Token> ParseLine(string line)
+        {
+            var tail = line;
+            var result = new List<Token>();
+
+            while (tail.Length > 0)
+            {
+                bool success = false;
+                foreach (var regexProbe in regexTable)
+                {
+                    var match = Regex.Match(tail, regexProbe.Key);
+                    if(match.Success)
+                    {
+                        result.Add(new Token
+                        {
+                            Type = regexProbe.Value,
+                            Value = match.Value
+                        });
+
+                        tail = tail
+                            .Substring(match.Value.Length)
+                            .TrimStart();
+                        success = true;
+                        break;
+                    }
+                }
+
+                if (success == false)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sicp/Sicp/Tests/Lisp/TokenizerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Sicp.Lisp;
+using Xunit;
+
+namespace Sicp.Tests.Lisp
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void Parse_Define()
+        {
+            var tokenizer = new Tokenizer();
+
+            List<Token> tokens = tokenizer.Parse("define y 5");
+
+            Assert.Equal(TokenType.Define, tokens[0].Type);
+            Assert.Equal(TokenType.Identifier, tokens[1].Type);
+            Assert.Equal("y", tokens[1].Value);
+            Assert.Equal(TokenType.Int, tokens[2].Type);
+            Assert.Equal("5", tokens[2].Value);
+        }
+
+        [Fact]
+        public void Parse_Plus()
+        {
+            var tokenizer = new Tokenizer();
+
+            List<Token> tokens = tokenizer.Parse("+ 3 5 8");
+
+            Assert.Equal(TokenType.Plus, tokens[0].Type);
+            Assert.Equal(TokenType.Int, tokens[1].Type);
+            Assert.Equal("3", tokens[1].Value);
+            Assert.Equal(TokenType.Int, tokens[2].Type);
+            Assert.Equal("5", tokens[2].Value);
+            Assert.Equal(TokenType.Int, tokens[3].Type);
+            Assert.Equal("8", tokens[3].Value);
+        }
+    }
+}
